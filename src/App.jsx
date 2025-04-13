8f309939256bdcda33d201a68eb5568b0ba80507
“Vishal <vishaldevkate1981@gmail.com>
2025-04-13 16:38:52 +0530

    initialised json server

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function App() {
+  const [questions, setQuestions] = useState([]);
+  const [response, setResponse] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [score, setScore] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  useEffect(() => {
+   fetch("http://localhost:3000/data")
+   .then((res)=> res.json())
+   .then((data)=>{
+    setQuestions(data.questions);
+    setLoading(false);
+    setResponse(Array(data.questions.length).fill([]));
+    setUserAnswers(Array(data.questions.length).fill([]));
+   })
+   .catch((err)=>{
+    console.log("error",err);
+    setError("failed to load questions");
+    setLoading(false);
+   })
+  }, [])
+  
   return (
     <div className='bg-emerald-50'>App</div>
   )
